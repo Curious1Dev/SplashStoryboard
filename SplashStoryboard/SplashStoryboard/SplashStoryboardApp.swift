6e43b1d91500be69ad86c310a22dadcb2a5c2502
Curious1Dev <54212571+Curious1Dev@users.noreply.github.com>
2023-10-13 21:08:02 +0530

    Xcodeproj added

---
+++ SplashStoryboard/SplashStoryboard/SplashStoryboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashStoryboardApp.swift
+//  SplashStoryboard
+//
+//  Created by Ani Adhikary on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SplashStoryboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
